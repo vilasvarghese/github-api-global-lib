58c59539a830e70e7ad5ffc734c5723a1071da11
vilasvarghese <49194047+vilasvarghese@users.noreply.github.com>
2021-06-08 16:39:15 +0530

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,7 +1,13 @@
 /*def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
-}*/
+}
+
+1. Call this without any parameter 
 
 def call() {
     sh "echo Hello World"
+}
+*/
+def call(String name, Stirng dt) {
+    sh "echo Hello World ${name} ${dt}"
 }
